935dedcb4e3860a57bddd5cc912716c34cb6315b
Anchey Peng <ancheypeng@gmail.com>
2021-02-08 15:05:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from 'react'
+import ScrollMagic from "scrollmagic"; // Or use scrollmagic-with-ssr to avoid server rendering problems
+import { TweenMax, TimelineMax } from "gsap"; // Also works with TweenLite and TimelineLite
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import './App.css'
+import { Box, Container, Grid, Typography } from '@material-ui/core'
+import lottie from 'lottie-web'
+import animData from './data';
+
+import Header from './components/header'
+
 
 function App() {
+
+  ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+  const controller = new ScrollMagic.Controller();
+
+  const animRef = useRef();
+
+  useEffect(() => {
+
+    var scrollPercent;
+    var accelamount = 0.4;
+    var delay = 0;
+   
+    var bgAnimScene = new ScrollMagic.Scene({
+      triggerElement: animRef.current,
+      duration: 4200,
+      triggerHook: 0,
+    })
+    // .addIndicators()
+    .setPin(animRef.current)
+    .addTo(controller);
+
+    var bgAnim = lottie.loadAnimation({
+      container: animRef.current,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      rendererSettings: {
+        progressiveLoad: false,
+        preserveAspectRatio: 'xMidYMid meet'
+      },
+      animationData: animData
+    });
+
+    bgAnim.addEventListener('DOMLoaded',
+    function (e) { 
+      console.log('Lottie Loaded');
+      TweenMax.to(animRef.current, 0.5, { opacity: 1 });
+    });
+
+    bgAnimScene.on('update', e => {
+      scrollPercent = 100 * window.scrollY / (window.document.body.offsetHeight - window.innerHeight);
+    });
+
+    setInterval(() => {
+      delay += (scrollPercent - delay) * accelamount;
+      bgAnim.goToAndStop((delay / 100) * 199, true)
+    }, 33.3)
+  });
+
+
+  const statsRef = useRef();
+
+  useEffect(() => {
+    // gsap.fromTo(statsRef.current, { opacity: 0, yPercent: 3 }, { duration: 0.5, opacity: 1, yPercent: 0 });
+
+    var timeline = new TimelineMax();
+    const statsAnim = TweenMax.fromTo(statsRef.current, 2, { opacity: 0, yPercent: 5 }, { opacity: 1, yPercent: 0 });
+    const statsAnim2 = TweenMax.to(statsRef.current, 2, { opacity: 0, yPercent: -5 });
+
+    timeline.add(statsAnim).add(statsAnim2, "+=1");
+
+
+    //Text animation Scene
+    const statisticsScene = new ScrollMagic.Scene({
+      duration: 800,
+      offset: 20,
+      triggerElement: statsRef.current,
+      triggerHook: 0
+    })
+      .setTween(timeline)
+      .setPin(statsRef.current)
+      .addTo(controller);
+      
+    console.log(statisticsScene);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header/>
+
+      <div ref={animRef} id="animation"></div>
+      
+      <div className="content">
+        <div className="intro h100">
+          <Typography variant="h1" id="title">Cornell Rocketry Team</Typography>
+        </div>
+
+        <Container ref={statsRef} className="statistics h100">
+          <div className="center">
+            <Box mb={12}>
+              <Typography variant="h2">
+                At a Glance
+              </Typography>
+            </Box>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+            >
+              <Grid item xs={3}>
+                <Typography variant="h2" className="statsNum">51</Typography>
+
+                <Typography variant="h5">Team Members</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="h2" className="statsNum">6</Typography>
+                <Typography variant="h5">Subteams</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="h2" className="statsNum">12</Typography>
+                <Typography variant="h5">Majors</Typography>
+              </Grid>
+              <Grid item xs={3}>
+                <Typography variant="h2" className="statsNum">17</Typography>
+                <Typography variant="h5">Rockets Launched</Typography>
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,227 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Menu, MenuItem, Box } from '@material-ui/core';
+
+import { Link } from 'react-router-dom';
+
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
+const transitionTime = 0.5;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  leftButtons: {
+    flexGrow: 1,
+  },
+  appBar: {
+    backgroundColor: 'black',
+    borderBottom: '1px solid black',
+    color: 'white',
+    transition: `all ${transitionTime}s`,
+    boxShadow: 'none',
+    paddingTop: 10,
+    paddingBottom: 10,
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: 120,
+      paddingRight: 120,
+    },
+    [theme.breakpoints.up('xl')]: {
+      paddingLeft: 190,
+      paddingRight: 190,
+    },
+    '&[data-active="true"]': {
+      backgroundColor: '#0C0C0C',
+      borderBottom: '1px solid #646464',
+      // color: 'black',
+      transition: `all ${transitionTime}s`,
+      // boxShadow: `0px 0px 10px 0px rgba(0,0,0,0.15)`,
+      // paddingTop: 5,
+      // paddingBottom: 5,
+    },
+  },
+  logo: {
+    height: 40,
+  },
+  appBarButtons: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none"
+    },
+    color: 'lightgrey',
+    transition: `all ${transitionTime}s`,
+    // '&[data-active="true"]': {
+    //   color: 'grey',
+    //   transition: `all ${transitionTime}s`,
+    // },
+
+  },
+  collapseAppBarButtons: {
+    flexGrow: 1,
+    [theme.breakpoints.up("md")]: {
+      display: 'none'
+    },
+  },
+  appBarIconButton: {
+    marginRight: -2,
+    marginLeft: -2,
+    [theme.breakpoints.down("sm")]: {
+      marginRight: -4,
+      marginLeft: -4,
+      "&:hover": {
+        backgroundColor: "transparent"
+      },
+    },
+  },
+  appBarIcon: {
+    color: 'lightgrey',
+    transition: `all ${transitionTime}s`,
+    // '&[data-active="true"]': {
+    //   color: 'grey',
+    //   transition: `all ${transitionTime}s`,
+    // },
+  },
+  appBarButton: {
+    marginLeft: 15,
+    marginRight: 15,
+    textTransform: 'none',
+    fontSize: '1.1rem',
+  },
+  homeButton: {
+    color: 'white',
+    transition: `all ${transitionTime}s`,
+    // '&[data-active="true"]': {
+    //   color: 'white',
+    //   transition: `all ${transitionTime}s`,
+    // },
+  }
+}));
+
+export default function Header({ active }) {
+  const classes = useStyles();
+
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      if (isScrolled !== scrolled) {
+        console.log(!scrolled);
+        setScrolled(!scrolled);
+      }
+    };
+
+    document.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    }
+
+  }, [active, scrolled]);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        className={classes.appBar}
+        data-active={active ? "true" : scrolled.toString()}
+      //position={active ? "static" : "fixed"}
+      >
+        <Toolbar>
+          <div className={classes.collapseAppBarButtons}>
+            <Box ml={3}>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleClick}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+
+              <MenuItem component={Link} to={'/'}>
+                Home
+                </MenuItem>
+              {['Team', 'Rockets', 'Sponsors', 'Outreach', 'Blog', 'Contact'].map((text) => (
+
+                <MenuItem component={Link} to={'/'}>
+                  {/* <Link to={`/${text.toLowerCase()}`} key={text}> */}
+                  {text}
+                  {/* </Link> */}
+                </MenuItem>
+
+              ))}
+            </Menu>
+          </div>
+
+          <div className={`${classes.leftButtons} ${classes.appBarButtons}`} data-active={active ? "true" : scrolled.toString()}>
+            <IconButton>
+              <img src="crt.png" alt="logo" className={classes.logo} />
+            </IconButton>
+            {/* <Link to="/"> */}
+            <Button
+              className={`${classes.homeButton} ${classes.appBarButton}`}
+              data-active={active ? "true" : scrolled.toString()}
+              component={Link} to={'/'}
+            >
+              Home
+              </Button>
+            {/* </Link> */}
+            {['Team', 'Rockets', 'Sponsors'].map((text) => (
+              <Button color="inherit" className={classes.appBarButton} component={Link} to={'/'}>
+                {text}
+              </Button>
+
+            ))}
+          </div>
+
+          <div>
+            <IconButton className={classes.appBarIconButton} target="_blank" href={"https://facebook.com/stemandbuds"}>
+              <FacebookIcon className={classes.appBarIcon} data-active={active ? "true" : scrolled.toString()} />
+            </IconButton>
+            <IconButton className={classes.appBarIconButton} target="_blank" href={"https://instagram.com/stemandbuds"}>
+              <InstagramIcon className={classes.appBarIcon} data-active={active ? "true" : scrolled.toString()} />
+            </IconButton>
+            <IconButton className={classes.appBarIconButton} target="_blank" href={"https://www.youtube.com/channel/UCObQHffPzd3jmg5Uyld0ELQ"}>
+              <YouTubeIcon className={classes.appBarIcon} data-active={active ? "true" : scrolled.toString()} />
+            </IconButton>
+          </div>
+
+          <div className={classes.appBarButtons} data-active={active ? "true" : scrolled.toString()}>
+            <Button color="inherit" className={classes.appBarButton} component={Link} to={'/'}>
+              Contact
+            </Button>
+            {/* <Button color="inherit" className={classes.appBarButton} component={Link} to={'/'}>
+              Donate
+            </Button> */}
+            <Button variant="contained" color="secondary" className={classes.appBarButton} component={Link} to={'/'}>
+              Apply
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
